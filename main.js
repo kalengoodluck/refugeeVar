b4c4610272a441e6dd08f2997a307e352e187ffb
kalengoodluck <kalen.goodluck@journalism.cuny.edu>
2018-03-19 13:07:38 -0400

    main.js var

---
+++ main.js
@@ -0,0 +1,11 @@
+//declare all global variables//
+
+var numberFamilymembers = x;
+var EnglishSpeakingMembers = y;
+var oneTimegrant2018 = numberFamilymembers * 1000;
+var monthlyExpensesSingle = 800;
+var monthlyExpensesCouple = 950;
+var monthlyExpensesThree = 1000;
+var monthlyExpensesFour = 1100;
+var monthlyExpensesFive = 1200;
+var NGO = " 'HelpUnlimited', 'BetterTomorrow','NewStart','HomeSafe','NewHome'";
